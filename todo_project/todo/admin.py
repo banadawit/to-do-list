f6da8bffd1ce1e9d2e6fec9585126f026c9ced6c
banadawit <banadawithunde@gmail.com>
2024-12-23 23:45:53 +0300

    correction

--- todo_project/todo/admin.py
+++ todo_project/todo/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
 from .models import Task, Category
 
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'category', 'completed', 'created_at')
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+admin.site.register(Task)
+admin.site.register(Category)
